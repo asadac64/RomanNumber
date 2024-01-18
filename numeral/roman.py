f058ee863b25c44f86831a6fe0691d4d70a89b26
Asada <asadac64@nu.ac.th>
2024-01-18 09:02:43 +0700

    initialize project

---
+++ numeral/roman.py
@@ -0,0 +1,4 @@
+"""The roman numeral module"""
+
+def to_roman():
+    """Convert Hindo_arabic to Roman number"""
---
+++ tests/numeral/test_numeral.py
@@ -0,0 +1,5 @@
+"""Test cases for Roman numeral"""
+
+from numeral.roman import to_roman
+def test_calling_function():
+    """Call a roman numeral function"""
