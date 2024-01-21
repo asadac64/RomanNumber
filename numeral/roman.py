fef4e4c1246964e517518d3f7c358eb8181599ab
Asada <asadac64@nu.ac.th>
2024-01-21 15:38:10 +0700

    arabic to roman

--- numeral/roman.py
+++ numeral/roman.py
@@ -3,5 +3,23 @@
 class NumberOutOfRange(ValueError):
     """An  extention"""
     
-def to_roman():
+def to_roman(num):
     """Convert Hindo_arabic to Roman number"""
+    I = ['','I','II','III','IV','V','VI','VII','VIII','IX'] #เลขหลักหน่วย 0-9
+    X = ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'] #เลขหลักสิบ 10,20,...,90
+    C = ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'] # เลขหลักร้อย 100,200,...,900
+    M = ['','M','MM','MMM'] # เลขหลักพัน 1000-3000
+    
+
+    num_1000 = M[num // 1000]
+    num_100 = C[(num % 1000) // 100]
+    num_10 = X[(num % 100) // 10]
+    num_1 = I[num % 10]
+    
+    roman = num_1000 + num_100 + num_10 + num_1
+    
+    return roman
+
+   
+
+
--- tests/numeral/test_numeral.py
+++ tests/numeral/test_numeral.py
@@ -1,5 +1,42 @@
 """Test cases for Roman numeral"""
+# Standard library
 
+#3rd Part library
+import pytest
+
+# Project library
 from numeral.roman import to_roman
-def test_calling_function():
-    """Call a roman numeral function"""
+from numeral.roman import NumberOutOfRange
+
+    
+@pytest.mark.parametrize(
+    "arabic_num, roman_num",
+    [
+        #Base case
+        (1, 'I'),
+        (5, 'V'),
+        (10,'X'),
+        (50,'L'),
+        (100,'C'),
+        (500,'D'),
+        (1000,'M'),
+        
+        # Additive case
+        (2,'II'),
+        (3,'III'),
+        (6,'VI'),
+        
+        # Subtraction case
+        (4,'IV'),
+        (9,'IX'),
+    ]
+)
+def test_to_roman(arabic_num, roman_num):
+    # Arrange
+    
+    # Act
+    result = to_roman(arabic_num)
+    
+    # Assert
+    assert result == roman_num
+    
